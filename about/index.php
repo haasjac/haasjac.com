1063b3e45f94bfd7b5ec571096e37cef4d5041d8
Jake Haas <haasjac@gmail.com>
2016-12-21 13:45:42 -0500

    about fix

--- about/index.php
+++ about/index.php
@@ -54,7 +54,8 @@
 					</span>
 					<div class="cardDescription">
 						University of Michigan<br>
-						Ann Arbor<br><br>
+						Ann Arbor, April 2017<br>
+						<br>
 				    	Computer Science BSE
 					</div>
 					<div class="cardLinks">
@@ -68,67 +69,6 @@
 				</div>
 			</div>
 		</div>
-		
-		<!--div class="about">
-			<img style="float:left;" src="jake_portrait.jpg" width="350px" height="525px;" />
-			
-			<div style="width: 550px; float: left;">
-				<div class="cardHeader">
-					<div class="cardHeaderTitleBox">
-						About
-					</div>
-					<div class="cardHeaderDescription">
-						I first learned to program when my friend showed me you can make games on graphing calculators.
-				        I love to work in teams to compete, whether it's programming an ardunio to fly a homemade blimp,
-				        making a robot that can play soccer, or making an original game in 48 hours, I enjoy working hard
-				        on a project and feeling accomplished for creating something with my team.			
-					</div>
-				</div>
-				
-				<div class="cardHeader">
-					<div class="cardHeaderTitleBox">
-						Education
-					</div>
-					<div class="cardHeaderDescription">
-						University of Michigan, Ann Arbor<br>
-				    	Bachelors in Science Engineering, Computer Science Engineering		
-					</div>
-				</div>
-			</div>
-		</div-->
-		<!-- table class="about">
-		<tr>
-		<td>
-		<img style="float:right;" src="jake_portrait.jpg" width="350px" height="525px;" />
-		</td>
-		<td>
-		<ul>
-		    <li class="title">About</li>
-		        <li>I first learned to program when my friend showed me you can make games on graphing calculators.
-		        I love to work in teams to compete, whether it's programming an ardunio to fly a homemade blimp,
-		        making a robot that can play soccer, or making an original game in 48 hours, I enjoy working hard
-		        on a project and feeling accomplished for creating something with my team. </li>
-		    	<br>
-		    <li class="title">Education</li>
-			    <li>University of Michigan, Ann Arbor</li>
-		    	<li>Bachelors in Science Engineering, Computer Science Engineering</li>
-		        <br>
-		    <li class="title">Computer Science</li>
-		    	<li>EECS 280 - Programming and Data Structures</li>
-		        <li>EECS 281 - Data Structures and Algorithms</li>
-		        <li>EECS 370 - Intro to Computer Organization</li>
-		        <li>ARTDES 372 - Video Games</li>
-		        <br>
-		    <li class="title">Mathematics</li>
-		        <li>MATH 215 - Calculus III</li>
-		        <li>MATH 216 - Calculus IV</li>
-		        <li>PHYSICS 140 - Mechanical Physics </li>
-		        <li>PHYSICS 240 - Electrical and Magnetic</li>
-		        <li>STATS 250 - Introduction to Stats</li>
-		</ul>
-		</td>
-		</tr>
-		</table-->
 	</section>
 	
 	<?php include($_SERVER['DOCUMENT_ROOT'] . '/footer.html'); ?>
