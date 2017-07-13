dc6b7d44406923a59d8f689ebdf7272196da7974
Jake Haas <haasjac@gmail.com>
2017-07-12 23:59:37 -0400

    calls

--- gatriex.com/gatriex.js
+++ gatriex.com/gatriex.js
@@ -1,4 +1,7 @@
 //global variables
+var version;
+var summonerData;
+var leagueData;
 var navButtonArrows = [ "&#9654", "&#9654", "&#9654", "&#9654", "&#9654", "&#9654"];
 var navButtonNames;
 var navigation = {length: {0:0}, Name: {0:""}, linkName: {0:""}};
@@ -8,6 +11,51 @@ $(function () {
 	getButtons();
 	myDate();
 });
+
+function getURL() {	
+	
+	//LEAGUE INFO
+	var urlName = "Gatriex";//getURLVariable("SummonerName");
+	if (urlName == "") {
+		document.getElementById("League").innerHTML = "<br>Welcome to Gatriex!";
+		document.getElementById("SummonerIcon").setAttribute('src', "/images/Logo.png");
+	} else {
+		value = 'value = "' + urlName + '"'
+		setURL("Version");
+		
+		$.ajax({
+			url: "Call.php?url=https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key=";
+			success: function(data) {
+				version = $.parseJSON(data)[0];
+				console.log(version);
+			},
+			error: function(xhr, status, error) {
+				console.log(error);
+			}
+		});
+		
+		$.ajax({
+			url: "Call.php?url=https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + userNoSpace + "?api_key=";
+			success: function(data) {
+				summonerData = $.parseJSON(data);
+				console.log(summonerData);
+			},
+			error: function(xhr, status, error) {
+				console.log(error);
+			}
+		});
+		
+		$.ajax({
+			url: "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/" + summonerData.id + "?api_key=";
+			success: function(data) {
+				leagueData = $.parseJSON(data);
+				console.log(leagueData);
+			},
+			error: function(xhr, status, error) {
+				console.log(error);
+			}
+		});
+}
 
 //gets navigation buttons
 function getButtons() {
