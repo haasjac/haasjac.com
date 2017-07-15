c594adbbc16921524660f46eea2d136ed576830f
Jake Haas <haasjac@gmail.com>
2017-07-15 16:19:52 -0400

    asdf

--- gatriex.com/credentials.php
+++ gatriex.com/credentials.php
@@ -3,5 +3,5 @@ $servername = "localhost";
 $username = "haasjacc_root";
 $password = "trumpet77697";
 $dbname = "haasjacc_gatriex";
-$api_token = 'RGAPI-946f2254-9f5d-4e8b-aa63-93d0ac5b7c24';
+$api_token = 'RGAPI-a0983d81-f3e3-410e-8b79-b9505168a18b';
 ?>
