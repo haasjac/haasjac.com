2de7b7a30c5bcb6137049ca5ac9bf0ad64c80d67
Jacob Haas <haasjac@umich.edu>
2017-08-01 21:33:42 -0400

    cred

--- gatriex.com/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$servername = "localhost";
-$username = "haasjacc_root";
-$password = "trumpet77697";
-$dbname = "haasjacc_gatriex";
-$api_token = 'RGAPI-a0983d81-f3e3-410e-8b79-b9505168a18b';
-?>
