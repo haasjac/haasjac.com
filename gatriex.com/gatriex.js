7b0d731ca4c42302b49063cf52941a5d48837cf1
Jake Haas <haasjac@gmail.com>
2017-08-09 20:06:49 -0400

    asdf

--- gatriex.com/gatriex.js
+++ gatriex.com/gatriex.js
@@ -20,7 +20,8 @@ function getVersion() {
 		url: "Call.php?url=https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key=",
 		//url: "Call.php?url=https://global.api.riotgames.com/api/lol/static-data/NA/v1.2/versions?api_key=",
 		success: function(data) {
-			version = data[0];
+			console.log($.parseJSON(data));
+			version = $.parseJSON(data)[0];
 			getId();
 		},
 		error: function(xhr, status, error) {
